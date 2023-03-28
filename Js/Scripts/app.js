bbd9cdbc3c9f510731eb1b9cb3195a8f019be60e
Luis Taliercio <taliercioluis1@gmail.com>
2023-03-27 21:55:07 -0300

    Add function to read dishes.json and charge it to the DOM automatically, fix and add styles, change hrefs of the navbar categories

--- Js/Scripts/app.js
+++ Js/Scripts/app.js
@@ -12,6 +12,7 @@ console.log(JSON.parse(jsonString))
 */ 
 
 function start(){
+
 const editInfo = document.getElementById("editInfo")
 const mainDiv = document.getElementById("infoDiv")
 const editInfoDiv = document.getElementById("editInfoDiv")
@@ -52,23 +53,46 @@ descripcionPDia.addEventListener('keyup',(event) => {
     document.getElementById('presentacionDesc').innerHTML = inputText;
 })
 
-function scrollFunction1(){
-    let e = document.getElementById("pescados");
-        e.scrollIntoView({
-          block: 'start',
-          behavior: 'smooth',
-          inline: 'start'
+//Lee platos.json y los carga en sus correspondientes divs en el menu textual, funciona bien pero tendria que optimizar la carga de los datos para no recorrer todos los platos cada vez que avanzo de categoria.
+const cargarPlatos = async () =>{
+    try {
+        let respuesta = await fetch('./Data/Platos.json')
+
+        const datos = await respuesta.json();
+
+        //Mapeo el arreglo datos y paso a cats los nombres de las categorias, podria que al realizar esto ya pasar los platos a un arreglo para cada categoria.
+        let cats = datos.map((dato) => {
+            let categoria = dato.cat;
+            return categoria;
         });
-}
 
+        //Por cada categoria recorro los platos y los cargo en su div previamente obtenido por id
+        cats.forEach(cate => {
+            let divComida = document.getElementById(cate.toLowerCase())
+            divComida.innerHTML=''
+            //let html = `<div class="categoria" id="${cate.toLowerCase()}"> <h3>${cate}</h3>`;
+            let html = `<h3>${cate}</h3>`
+            let comidas = '';
+            datos.forEach(Plato => {
+                if(Plato.cat === cate){
+                    
+                    comidas += (comidas.substring(0,4)==='<ul>' ? `` : `<ul>`) + `<li> <h4>${Plato.nombre}</h4> <button value=${Plato.id}>${Plato.precio} <img src="./Iconos/shoppingCart/apple-touch-icon.png" style="width: 24px; height: 24px;" alt=""> </button> </li>`
+                }
+            });
+            /*Si se abrio una lista con platos se cierra, sino se deja el div vacio*/
+            (comidas.substring(0,4)==='<ul>' ? '</ul>' : '');
+            
+            html += comidas
+            divComida.innerHTML = html;
+        });
+        
+    } catch (error) {
+        
+    }
+} 
+    
+    cargarPlatos();
 
-let p1 = new Plato('Mondongo','Mondongo a la espanola con papas',1500.00,1,'Carnes Rojas')
-
-console.log(p1.presentacion());
-
-p1.setPlatoDia(0);
-
-console.log(p1.presentacion());
 }
 
 /*

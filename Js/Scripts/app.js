469afb0335eb6c7eecf4fd5128aa70e63a89d2d1
Luis Taliercio <taliercioluis1@gmail.com>
2023-04-09 13:16:15 -0300

    Refactor scripts, Change styles

--- Js/Scripts/app.js
+++ Js/Scripts/app.js
@@ -3,17 +3,6 @@ window.onload = start();
 import {Plato} from '../Class/Plato.js'
 
 function start(){
-   
-//En index page
-const editInfo = document.getElementById("editInfo")
-const mainDiv = document.getElementById("infoDiv")
-const editInfoDiv = document.getElementById("editInfoDiv")
-const categoriesDiv = document.querySelector(".opciones")
-const editBtn = document.getElementById("edit")
-
-//En edit page.
-let nombrePlatoDia = document.getElementById('inputPlatoDia');
-let descripcionPDia = document.getElementById('descripcionPlatoDia');
 
 //Carrito
 let carrito = [];
@@ -24,40 +13,6 @@ const inputSubT = document.getElementById('subtotal');
 
 //Boton para ordenar al costado del inputSubTotal
 const orderBtn = document.getElementById('order')
-
-//Al cliquear Edit se muestra la seccion para ingresar la info del plato del dia
-editInfo.addEventListener('click',() => {
-    mainDiv.classList.add('isInactive');
-    categoriesDiv.classList.add('isInactive');
-    editInfoDiv.classList.remove('isInactive');
-})
-
-//Al cliquear Edit en la seccion de info del plato del dia se muestra el index con la info actualizada
-editBtn.addEventListener('click',() => {
-    let nombrePlatoDia = document.getElementById('inputPlatoDia').value;
-    let descriptionPlatoDia = document.getElementById('descripcionPlatoDia').value;
-
-    mainDiv.classList.remove('isInactive');
-    categoriesDiv.classList.remove('isInactive');
-    editInfoDiv.classList.add('isInactive');
-    
-    console.log(nombrePlatoDia + ', Nombre plato');
-    console.log(descriptionPlatoDia + ', Descripcion plato');
-
-    document.getElementById('nombrePDia').innerHTML=nombrePlatoDia;
-    document.getElementById('descripcionPDia').innerHTML = descriptionPlatoDia;
-    }
-)
-
-//Muestra en tiempo real la info que se va escribiendo acerca del plato del dia en la Edit page
-nombrePlatoDia.addEventListener('keyup',(event) => {
-    let inputText = nombrePlatoDia.value;
-    document.getElementById('presentacionNombre').innerHTML = inputText; 
-})
-descripcionPDia.addEventListener('keyup',(event) => {
-    let inputText = document.getElementById('descripcionPlatoDia').value;
-    document.getElementById('presentacionDesc').innerHTML = inputText;
-})
 
 //Lee platos.json y los carga en sus correspondientes divs en el menu textual, funciona bien pero tendria que optimizar la carga de los datos para no recorrer todos los platos cada vez que avanzo de categoria.
 const cargarPlatos = async () =>{
@@ -101,64 +56,68 @@ const agregarCarrito = (plato) =>{
     carrito.push(plato);
     subTotal += plato.precio;
     inputSubT.setAttribute('placeholder',`$${subTotal}`);
+    console.log(carrito);
 }
 
 //Al realizar la orden el subtotal vuelve a 0
+orderBtn.onclick = ()=>{
+    console.log('Carrito Vaciado, Orden pedida, Subtotal restaurado a 0');
+    inputSubT.setAttribute('placeholder',"$0");
+    carrito = [];
+    subTotal = 0;
+}
+
 orderBtn.addEventListener('click',(event)=>{
     inputSubT.setAttribute('placeholder',``);
 })
 
 const cargarPlatosCreate = async() => {
-    let respuesta = await fetch('./Data/Platos.json');
+    let respuesta = await fetch('./Data/food.json');
     let platos = await respuesta.json();
-    
-    //Mapeo los platos y obtengo solamente las categorias de cada uno para su posterior filtrado
-    let categorias = platos.map((plato) => plato.cat)
 
-    //Retorna las categorias donde su primer indice concuerda con el del elemento actual, evitando asi elementos repetidos
+    //Mapeo los platos y obtengo solamente las categorias de cada uno para su posterior filtrado
+    let categorias = platos.map((plato) => plato.nombreCat)
+
+    //Retorna las categorias donde su primer indice concuerda con el del elemento actual, evitando asi elementos repetidos, si Pescados con indice 14 se encuentra su primer valor al indice 3 no lo pasa
     categorias = categorias.filter((valor,indice) => {
         return categorias.indexOf(valor) === indice
-    })
+    })  
 
     //Por cada categoria listada obtengo su div en el html y creo una lista ul
     categorias.forEach((cat) => {
         let divCat = document.getElementById(cat.toLowerCase());
-        let listaCat = document.createElement('ul')
+        if(divCat != null){
+            let listaCat = document.createElement('ul')
 
-        //Por cada plato, comparo su categoria con la actual y si coincide le creo un item li en la lista ul
-        platos.forEach((plato) =>{
-            if(plato.cat === cat){
-                let platoItem = document.createElement('li');
+            //Por cada plato, comparo su categoria con la actual y si coincide le creo un item li en la lista ul
+            platos.forEach((plato) =>{
+                if(plato.nombreCat === cat){
+                    let platoItem = document.createElement('li');           
+                    let h4 = document.createElement('h4');
+                    h4.textContent = plato.nombre;          
+                    let button = document.createElement('button');
+                    button.textContent = `$${plato.precio}`;
+                    button.setAttribute('value',plato.idPlato); 
+                    button.addEventListener('click',()=>{console.log(`boton cliqueado ${button.value}`);agregarCarrito (plato)})       
+                    let shoppingCartImg = document.createElement('img');
+                    shoppingCartImg.setAttribute('src',"./Iconos/shoppingCart/apple-touch-icon.png")
+                    button.appendChild(shoppingCartImg);            
+                    platoItem.appendChild(h4);
+                    platoItem.appendChild(button);
 
-                let h4 = document.createElement('h4');
-                h4.textContent = plato.nombre;
-
-                let button = document.createElement('button');
-                button.textContent = `$${plato.precio}`;
-                button.setAttribute('value',plato.id); 
-                button.addEventListener('click',()=>{console.log(`boton cliqueado ${button.value}`);agregarCarrito(plato)})
-
-                let shoppingCartImg = document.createElement('img');
-                shoppingCartImg.setAttribute('src',"./Iconos/shoppingCart/apple-touch-icon.png")
-                button.appendChild(shoppingCartImg);
-
-                platoItem.appendChild(h4);
-                platoItem.appendChild(button);
-                
-                listaCat.appendChild(platoItem);
-            }
-            
-        })
+                    listaCat.appendChild(platoItem);
+                } 
+            })
         divCat.appendChild(listaCat)
+        }
     });
 
 }
-    cargarPlatosCreate()
-    
-    //Para chequear que se agreguen los productos al carrito
-    /*setInterval(() => {
-        console.log('Carrito actual');
-        console.log(carrito);
-    }, 10000);*/
-}
 
+    const obtenerFoodFile = async() =>{
+        //console.log(path.join(__dirname));
+        //fs.readFile(path.join(__dirname))
+    }
+    cargarPlatosCreate()
+    obtenerFoodFile()
+}
